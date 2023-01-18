15fc27ce4926d2fa7fcd49d28b4425b919877f7d
Lailah Berry <lailahnicole614@gmail.com>
2023-01-18 10:50:52 -0800

    added state

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
-/* Imports */
+/* Imports and Get DOM Elements */
 const desertDropdown = document.getElementById('desert-dropdown');
 const temperateDropdown = document.getElementById('temperate-dropdown');
 const tropicalDropdown = document.getElementById('tropical-dropdown');
 const slogansEl = document.getElementById('slogans');
 const sloganInputEl = document.getElementById('slogan-input');
 const sloganButton = document.getElementById('slogan-button');
-
-/* Get DOM Elements */
-
+const desertEl = document.getElementById('desert');
+const temperateEl = document.getElementById('temperate');
+const tropicalEl = document.getElementById('tropical');
 /* State */
+let desertCount = 0;
+let temperateCount = 0;
+let tropicalCount = 0;
 
+let slogans = [];
 /* Events */
 
 /* Display Functions */
