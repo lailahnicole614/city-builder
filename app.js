09fd722f05a1dc2636309818e1e4b3885c59c7ba
Lailah Berry <lailahnicole614@gmail.com>
2023-01-18 10:07:25 -0800

    added imports

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 /* Imports */
+const desertDropdown = document.getElementById('desert-dropdown');
+const temperateDropdown = document.getElementById('temperate-dropdown');
+const tropicalDropdown = document.getElementById('tropical-dropdown');
+const slogansEl = document.getElementById('slogans');
+const sloganInputEl = document.getElementById('slogan-input');
+const sloganButton = document.getElementById('slogan-button');
 
 /* Get DOM Elements */
 
